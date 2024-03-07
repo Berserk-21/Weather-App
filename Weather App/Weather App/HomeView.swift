faadad8e1f75522b72e8b4710066a74ec305a832
Berserk-21 <cryptogekkofr@protonmail.com>
2024-03-07 12:03:14 +0100

    Move the work logic inside the viewModel, reactify the views of HomeView

--- Weather App/Weather App/HomeView.swift
+++ Weather App/Weather App/HomeView.swift
@@ -32,14 +32,15 @@ final class HomeView: UIView {
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        setupUI()
+        setupLayout()
         
-        subscribe()
+        setupBindings()
         
         viewModel.fetchWeatherData()
     }
     
-    private func setupUI() {
+    private func setupLayout() {
+        
         cityLabel.font = UIFont.systemFont(ofSize: 32.0, weight: .medium)
         temperatureLabel.font = UIFont.systemFont(ofSize: 80.0, weight: .light)
         weatherCodeLabel.font = UIFont.systemFont(ofSize: 24.0, weight: .medium)
@@ -49,152 +50,33 @@ final class HomeView: UIView {
     
     // MARK: - Binding Methods
     
-    private func subscribe() {
-            
-        viewModel.weatherForecast
-            .observe(on: MainScheduler.instance)
-            .subscribe { [weak self] forecast in
-                self?.setupCurrentWeather(with: forecast)
-            } onError: { error in
-                print(error.localizedDescription)
-            } onCompleted: {
-                print("did complete forecast")
-            }
+    private func setupBindings() {
+        
+        viewModel.currentTemperature?
+            .compactMap { $0 }
+            .bind(to: temperatureLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        viewModel.minMaxTemperature?
+            .compactMap { $0 }
+            .bind(to: maxMinTemperatureLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        viewModel.weatherCodeString?
+            .compactMap { $0 }
+            .bind(to: weatherCodeLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        viewModel.backgroundImage?
+            .compactMap { $0 }
+            .bind(to: backgroundImageView.rx.image)
+            .disposed(by: disposeBag)
+        
+        viewModel.cityString?
+            .compactMap { $0 }
+            .bind(to: cityLabel.rx.text)
             .disposed(by: disposeBag)
     }
-    
-    private func setupCurrentWeather(with forecast: WeatherForecastModel) {
-        
-        cityLabel.text = "Montpellier"
-        temperatureLabel.text = "\(forecast.current.temperature_2m)°"
-        weatherCodeLabel.text = descriptionForWeatherCode(forecast.current.weather_code)
-        maxMinTemperatureLabel.text = describeExtremeTemperatures(with: forecast)
-        backgroundImageView.image = getBackgroundImageForWeatherCode(forecast.current.weather_code)
-    }
-    
-    // MARK: - Helper Methods
-    
-    func describeExtremeTemperatures(with forecast: WeatherForecastModel) -> String {
-        
-        var string = ""
-        
-        if let maxTemp = forecast.hourly.maxTemperature?.rounded(.down) {
-            string += "↑\(maxTemp.rounded())°"
-        }
-        
-        if let minTemp = forecast.hourly.minTemperature?.rounded(.down) {
-            if !string.isEmpty {
-                string += " "
-            }
-            
-            string += "↓\(minTemp)°"
-        }
-        
-        return string
-    }
-    
-    func getBackgroundImageForWeatherCode(_ code: Int) -> UIImage? {
-        
-        switch code {
-        case 0:
-            return UIImage(named: "clear_sky")
-        case 1...3:
-            return UIImage(named: "partly_cloudy")
-        case 4:
-            return UIImage(named: "overcast")
-        case 10, 20...29, 45, 48:
-            return UIImage(named: "mist")
-        case 50...69, 80...99:
-            return UIImage(named: "rainy")
-        case 70...79:
-            return UIImage(named: "snowfall")
-        default:
-            return UIImage(named: "moon")
-        }
-    }
-    
-    func descriptionForWeatherCode(_ code: Int) -> String {
-        switch code {
-        case 0:
-            return "Clear sky"
-        case 1...3:
-            return "Partly cloudy"
-        case 4:
-            return "Overcast"
-        case 10, 20...29, 45, 48:
-            return "Mist"
-        case 50...69, 80...99:
-            return "Rain"
-        case 70...79:
-            return "Snowfall"
-        default:
-            return "Unknown weather condition"
-        }
-    }
-    
-//    private func weatherCodeInterpretation(from code: Int) -> String {
-//        switch code {
-//        case 0:
-//            return "Clear"
-//        case 1:
-//            return "Mostly Clear"
-//        case 2:
-//            return "Partly Cloudy"
-//        case 3:
-//            return "Cloudy"
-//        case 45:
-//            return "Fog"
-//        case 48:
-//            return "Freezing Fog"
-//        case 51:
-//            return "Light Drizzle"
-//        case 53:
-//            return "Drizzle"
-//        case 55:
-//            return "Heavy Drizzle"
-//        case 56:
-//            return "Light Freezing Drizzle"
-//        case 57:
-//            return "Freezing Drizzle"
-//        case 61:
-//            return "Light Rain"
-//        case 63:
-//            return "Rain"
-//        case 65:
-//            return "Heavy Rain"
-//        case 66:
-//            return "Light Freezing Rain"
-//        case 67:
-//            return "Freezing Rain"
-//        case 71:
-//            return "Light Snow"
-//        case 73:
-//            return "Snow"
-//        case 75:
-//            return "Heavy Snow"
-//        case 77:
-//            return "Snow Grains"
-//        case 80:
-//            return "Light Rain Shower"
-//        case 81:
-//            return "Rain Shower"
-//        case 82:
-//            return "Heavy Rain Shower"
-//        case 85:
-//            return "Snow Shower"
-//        case 86:
-//            return "Heavy Snow Shower"
-//        case 95:
-//            return "Thunderstorm"
-//        case 96:
-//            return "Hailstorm"
-//        case 99:
-//            return "Heavy Hailstorm"
-//        default:
-//            return "Unknown weather code"
-//        }
-//        
-//    }
     
 }
 
--- Weather App/Weather App/WeatherViewModel.swift
+++ Weather App/Weather App/WeatherViewModel.swift
@@ -16,6 +16,48 @@ final class WeatherViewModel {
     
     private let weatherService: OpenWeatherService
     
+    let temperatureFormatter = NumberFormatter()
+    
+    lazy var currentTemperature: Observable<String>? = {
+        return weatherForecast
+            .compactMap { [weak self] in
+                self?.formatTemperature($0.current.temperature_2m)
+            }
+            .asObservable()
+    }()
+    
+    lazy var minMaxTemperature: Observable<String>? = {
+        return weatherForecast
+            .compactMap { [weak self] in
+                self?.formatExtremeTemperatures(from: $0)
+            }
+            .asObservable()
+    }()
+    
+    lazy var weatherCodeString: Observable<String>? = {
+        return weatherForecast
+            .compactMap { [weak self] in
+                self?.descriptionForWeatherCode($0.current.weather_code)
+            }
+            .asObservable()
+    }()
+    
+    lazy var backgroundImage: Observable<UIImage>? = {
+        return weatherForecast
+            .compactMap { [weak self] in
+                self?.getBackgroundImageForWeatherCode($0.current.weather_code)
+            }
+            .asObservable()
+    }()
+    
+    lazy var cityString: Observable<String>? = {
+        return weatherForecast
+            .compactMap { _ in
+                return "Montpellier"
+            }
+            .asObservable()
+    }()
+    
     // BehaviosSubject emits an initial value (what is set in the instanciation).
     // PublishSubject emits only value after the subscription is made.
 //    var weatherForecast: BehaviorSubject<WeatherForecast?> = BehaviorSubject(value: nil)
@@ -26,6 +68,8 @@ final class WeatherViewModel {
     
     init() {
         weatherService = OpenWeatherService()
+        
+        fetchWeatherData()
     }
     
     // Fetch weather forecast in a property, the views must observe it to get forecast events.
@@ -42,4 +86,136 @@ final class WeatherViewModel {
             .disposed(by: disposeBag)
     }
     
+    private func descriptionForWeatherCode(_ code: Int) -> String {
+        switch code {
+        case 0:
+            return "Clear sky"
+        case 1...3:
+            return "Partly cloudy"
+        case 4:
+            return "Overcast"
+        case 10, 20...29, 45, 48:
+            return "Mist"
+        case 50...69, 80...99:
+            return "Rain"
+        case 70...79:
+            return "Snowfall"
+        default:
+            return "Unknown weather condition"
+        }
+    }
+    
+    private func getBackgroundImageForWeatherCode(_ code: Int) -> UIImage? {
+        
+        switch code {
+        case 0:
+            return UIImage(named: "clear_sky")
+        case 1...3:
+            return UIImage(named: "partly_cloudy")
+        case 4:
+            return UIImage(named: "overcast")
+        case 10, 20...29, 45, 48:
+            return UIImage(named: "mist")
+        case 50...69, 80...99:
+            return UIImage(named: "rainy")
+        case 70...79:
+            return UIImage(named: "snowfall")
+        default:
+            return UIImage(named: "moon")
+        }
+    }
+    
+    private func formatTemperature(_ temperature: CGFloat) -> String? {
+        temperatureFormatter.roundingMode = .halfDown
+        temperatureFormatter.maximumFractionDigits = 0
+        
+        guard var temperatureString = temperatureFormatter.string(from: NSNumber(floatLiteral: temperature)) else { return nil }
+        
+        temperatureString += "°"
+        
+        return temperatureString
+    }
+    
+    private func formatExtremeTemperatures(from forecast: WeatherForecastModel) -> String {
+        
+        var string = ""
+        
+        if let maxTemp = forecast.hourly.maxTemperature, let maxTempString = formatTemperature(maxTemp) {
+            string += "↑\(maxTempString)"
+        }
+        
+        if let minTemp = forecast.hourly.minTemperature, let minTempString = formatTemperature(minTemp) {
+            if !string.isEmpty {
+                string += " "
+            }
+            
+            string += "↓\(minTempString)"
+        }
+        
+        return string
+    }
+    
+//    private func weatherCodeInterpretation(from code: Int) -> String {
+//        switch code {
+//        case 0:
+//            return "Clear"
+//        case 1:
+//            return "Mostly Clear"
+//        case 2:
+//            return "Partly Cloudy"
+//        case 3:
+//            return "Cloudy"
+//        case 45:
+//            return "Fog"
+//        case 48:
+//            return "Freezing Fog"
+//        case 51:
+//            return "Light Drizzle"
+//        case 53:
+//            return "Drizzle"
+//        case 55:
+//            return "Heavy Drizzle"
+//        case 56:
+//            return "Light Freezing Drizzle"
+//        case 57:
+//            return "Freezing Drizzle"
+//        case 61:
+//            return "Light Rain"
+//        case 63:
+//            return "Rain"
+//        case 65:
+//            return "Heavy Rain"
+//        case 66:
+//            return "Light Freezing Rain"
+//        case 67:
+//            return "Freezing Rain"
+//        case 71:
+//            return "Light Snow"
+//        case 73:
+//            return "Snow"
+//        case 75:
+//            return "Heavy Snow"
+//        case 77:
+//            return "Snow Grains"
+//        case 80:
+//            return "Light Rain Shower"
+//        case 81:
+//            return "Rain Shower"
+//        case 82:
+//            return "Heavy Rain Shower"
+//        case 85:
+//            return "Snow Shower"
+//        case 86:
+//            return "Heavy Snow Shower"
+//        case 95:
+//            return "Thunderstorm"
+//        case 96:
+//            return "Hailstorm"
+//        case 99:
+//            return "Heavy Hailstorm"
+//        default:
+//            return "Unknown weather code"
+//        }
+//
+//    }
 }
